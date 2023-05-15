d8423ba2b792d6e6297d5381841bf7f9df654052
parikshitc87 <parikshitchauhanqa@gmail.com>
2023-05-15 20:42:39 +0200

    Form validations, continued + 1

--- Page_Elements/Registration_Pages_Elements/registration_page_elements.py
+++ Page_Elements/Registration_Pages_Elements/registration_page_elements.py
@@ -1,5 +1,5 @@
 class RegistrationPage:
-	form_title = 'text = Create account >> nth = 0'
+	form_heading = 'text = Create account >> nth = 0'
 	create_button = 'text = Create account >> nth = 1'
 	first_name_label = 'text = First name'
 	first_name_input_field = "[placeholder=\"First name\"]"
@@ -13,8 +13,9 @@ class RegistrationPage:
 	confirm_password_input_field = "[placeholder = \"Repeat password\"]"
 	terms_of_use_checkbox = "[data-test-id=\"FormCheckboxButton\"]"
 	already_have_an_account_login = "text = Already have an account ?Log in"
-
-	page_elements = [form_title, create_button, first_name_label, first_name_input_field, last_name_label,
-	                 last_name_input_field, email_label, email_input_field,
-	                 password_label, password_input_field, confirm_password_label, confirm_password_input_field,
-	                 terms_of_use_checkbox, already_have_an_account_login]
+	page_elements = [
+		form_heading, create_button, first_name_label,
+		first_name_input_field, last_name_label, last_name_input_field, email_label, email_input_field,
+		password_label, password_input_field, confirm_password_label, confirm_password_input_field,
+		terms_of_use_checkbox, already_have_an_account_login
+	]
--- test_ui/test_registration_account_creation/test_registration_form_validations.py
+++ test_ui/test_registration_account_creation/test_registration_form_validations.py
@@ -1,30 +1,27 @@
-from playwright.sync_api import expect
-
-from Helper_Modules.Common_Functions.portal import login_with
 from Helper_Modules.Test_Data import data_gen
-from Page_Elements.Registration_Pages_Elements import registration_page_elements
 from Page_Elements.Registration_Pages_Elements.registration_page_elements import RegistrationPage
-from Page_Elements.login_page_elements import LoginPage
 
 
-def test_registration_form_element_presence(set_up):  # Registration Form - Test if the elements are loaded on the Registration form
+# Registration Form - Test if the elements are loaded on the Registration form
+def test_registration_form_element_presence(set_up):
 	page = set_up
 	flag = True
 	page.locator(RegistrationPage.email_input_field).fill("test")
 	for element in RegistrationPage.page_elements:
 		if page.locator(element).is_hidden():
 			flag = False
-	assert (flag == True)
+	assert flag is True
 
 
-# @pytest.mark.regression
-def test_registration_form_validations_1(set_up):  # Registration Form - Tests the presence of error messages on page
+# Registration Form - Test the presence of error messages on page
+def test_registration_form_blank_field_error_message(set_up):
 	page = set_up
-	page.get_by_role("button", name="Create account").click()
-	assert page.get_by_text("This field is required.").count() == 4
+	page.locator(RegistrationPage.create_button).click()
+	assert page.get_by_text("This field is required.").count() is 4
 
 
-def test_email_field_3(set_up):  # Registration Form - Email field - test set of valid emails
+# Registration Form - Email field - test set of valid emails
+def test_email_field_valid_emails(set_up):
 	page = set_up
 	flag = 0
 	for email in data_gen.valid_emails:
@@ -32,11 +29,12 @@ def test_email_field_3(set_up):  # Registration Form - Email field - test set of
 		if page.get_by_text("Please provide a valid email address.").count() == 0:
 			flag += 1
 		else:
-			assert False
+			print("Validation failed for ::" + " " + email)
 	assert len(data_gen.valid_emails) == flag
 
 
-def test_email_field_invalid_emails(set_up):  # Registration form - Email field - test set of invalid emails
+# Registration form - Email field - test set of invalid emails
+def test_email_field_invalid_emails(set_up):
 	page = set_up
 	flag = 0
 	for email in data_gen.invalid_emails:
@@ -50,3 +48,23 @@ def test_email_field_invalid_emails(set_up):  # Registration form - Email field 
 		else:
 			print("Validation failed for :: " + " " + email)
 	assert len(data_gen.invalid_emails) == flag
+
+
+# Registration Form - Test TOS and Privacy policy links
+def test_registration_form_tos_privacy_policy(set_up):
+	page = set_up
+	flag = False
+	with page.expect_popup() as popup_info:
+		page.get_by_role("link", name="terms of use").click()
+	page1 = popup_info.value
+	page.wait_for_url("https://portal-dev.dev.otc.workpage.io/terms-person?user=demo&password=portal")
+	if page1.get_by_role("heading", name="Terms of use AVACO – Private").is_visible():
+		flag = True
+	page1.close()
+	with page.expect_popup() as popup_info:
+		page.get_by_role("link", name="privacy policy").click()
+	page2 = popup_info.value
+	page.wait_for_url("https://portal-dev.dev.otc.workpage.io/terms-person?user=demo&password=portal")
+	if page2.get_by_role():
+		return
+	page2.close()
