57b81413df51c142989bf710a143e1a854b0ff62
parikshitc87 <parikshitchauhanqa@gmail.com>
2023-05-15 18:11:50 +0200

    Form validations, continued

--- test_ui/test_registration_account_creation/test_registration_form_validations.py
+++ test_ui/test_registration_account_creation/test_registration_form_validations.py
@@ -7,27 +7,24 @@ from Page_Elements.Registration_Pages_Elements.registration_page_elements import
 from Page_Elements.login_page_elements import LoginPage
 
 
-def test_registration_form_element_presence(set_up):
+def test_registration_form_element_presence(set_up):  # Registration Form - Test if the elements are loaded on the Registration form
 	page = set_up
 	flag = True
 	page.locator(RegistrationPage.email_input_field).fill("test")
 	for element in RegistrationPage.page_elements:
-		if page.locator(element).is_disabled():
+		if page.locator(element).is_hidden():
 			flag = False
 	assert (flag == True)
 
 
 # @pytest.mark.regression
-def test_registration_form_validations_1(set_up):  # Registers a person account
+def test_registration_form_validations_1(set_up):  # Registration Form - Tests the presence of error messages on page
 	page = set_up
 	page.get_by_role("button", name="Create account").click()
-	if page.get_by_text("This field is required.").count() == 4:
-		assert True
-	else:
-		assert False
+	assert page.get_by_text("This field is required.").count() == 4
 
 
-def test_email_field_3(set_up):  # test set of valid emails
+def test_email_field_3(set_up):  # Registration Form - Email field - test set of valid emails
 	page = set_up
 	flag = 0
 	for email in data_gen.valid_emails:
@@ -39,7 +36,7 @@ def test_email_field_3(set_up):  # test set of valid emails
 	assert len(data_gen.valid_emails) == flag
 
 
-def test_email_field_4(set_up):  # test set of invalid emails
+def test_email_field_invalid_emails(set_up):  # Registration form - Email field - test set of invalid emails
 	page = set_up
 	flag = 0
 	for email in data_gen.invalid_emails:
@@ -52,6 +49,4 @@ def test_email_field_4(set_up):  # test set of invalid emails
 		# page.wait_for_timeout(2000)
 		else:
 			print("Validation failed for :: " + " " + email)
-			continue
-	# assert False
 	assert len(data_gen.invalid_emails) == flag
